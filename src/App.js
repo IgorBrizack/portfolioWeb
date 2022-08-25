80902e8a2b4bb6108ec8346eaef7a048d9b51d8a
Igor Brizack <igor_brizack@hotmail.com>
2022-08-25 00:04:54 -0300

    novo portfolio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AboutMe from './Components/AboutMe';
+import Header from './Components/Header';
+import Projetos from './Components/Projetos';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AboutMe />
+      <Projetos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header () {
+  return (
+    <header>
+      <div>
+        <h1>Igor Brizack Desenvolvedor Front-End</h1>
+      </div>
+      <div>
+        <img height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" 
+        alt="github-con"/>
+        <img height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt="linkedin-icon"/>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
