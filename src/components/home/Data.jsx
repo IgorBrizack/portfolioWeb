24a1d1faa294700527ed10e54e91b0af39fc0259
Igor Brizack <igor_brizack@hotmail.com>
2023-03-14 10:48:38 -0300

    feat: skills component

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -64,7 +64,7 @@ const Data = () => {
 			</p>
 
 			<a href="#contact" className="button button--flex">
-				Diga Olá
+				Say Hello
 				<svg
                   class="button__icon"
                   xmlns="http://www.w3.org/2000/svg"
